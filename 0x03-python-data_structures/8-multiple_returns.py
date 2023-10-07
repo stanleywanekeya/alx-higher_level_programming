2732eb5de97096b1d12ebe6c42e0564fd77f3c9d
stanleywanekeya <stanleywanekeya1123@gmail.com>
2023-10-07 14:14:24 +0300

    Lists

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    size = len(sentence)
-    attr = size, sentence[0]
-    if size == 0:
-        return (size, None)
-    else:
-        return attr
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
