775fc5e18c0208f3edc66254e0c1380a5c07747b
stanleywanekeya <stanleywanekeya1123@gmail.com>
2023-10-09 08:40:29 +0300

    Bytecode

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <python.h>
+
+/**
+ * print_python_list_info - prints basic info of python list
+ * @p: Object list
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, alloc, i;
+	PyObject *obj;
+
+	size = Py_SIZE(p);
+	alloc = ((PyListObjext)p)->allocated;
+
+	prinf("[*] Size of Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; ++i)
+	{
+		printf("Element %d: ", i);
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", PyType(obj)->tp_name);
+	}
+}
