f1299213270294f2b42dd8258539f34cd6a4b598
stanleywanekeya <stanleywanekeya1123@gmail.com>
2023-10-11 19:26:52 +0300

    Fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/pthon3
 def multiply_by_2(a_dictionary):
     new_list = a_dictionary.copy()
-    list_key = new_list.keys()
+    list_key = list(new_list.keys())
     for i in list_key:
         new_list[i] *= 2
     return new_list
