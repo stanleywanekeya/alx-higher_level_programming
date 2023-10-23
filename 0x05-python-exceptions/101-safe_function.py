540f578a5335afba565db52beec2f163a72d3e6e
stanleywanekeya <stanleywanekeya1123@gmail.com>
2023-10-23 15:40:10 +0300

    Safe

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,6 +3,6 @@ def safe_function(fct, *args):
     try:
         res = fct(*args)
         return res
-    except Exception as error:
-        print("Exception: {}".format(error), file=sys.stderr)
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
         return None
