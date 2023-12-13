e790bedf6d98ab7887bcf4c2d4d2a5cacbdc7979
stanleywanekeya <stanleywanekeya1123@gmail.com>
2023-12-13 20:59:01 +0300

    all

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
--- Compute the avarage of score
-SELECT AVG(`score`) AS `avarage`
-FROM second_table;
+-- Computes the average score of all records in the table second_table in my MySQL server.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
