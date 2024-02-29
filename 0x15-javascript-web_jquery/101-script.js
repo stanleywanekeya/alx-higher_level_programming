5b9686a34ad79dc57ef26c6f57f5a6621cbc7164
stanleywanekeya <stanleywanekeya1123@gmail.com>
2024-02-29 19:18:47 +0300

    jquery

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -3,7 +3,7 @@ $('document').ready(function () {
     $('UL.my_list').append('<li>Item</li>');
   });
   $('DIV#remove_item').click(function () {
-    $('UL.my_list').remove();
+    $('UL.my_list li:last').remove();
   });
   $('DIV#clear_list').click(function () {
     $('UL.my_list').empty();
--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,3 +1,3 @@
-$('DIV#update_header').click(function() {
+$('DIV#update_header').click(function () {
   $('HEADER').text('New Header!!!');
 });
